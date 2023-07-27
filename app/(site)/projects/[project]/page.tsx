f871ccbb4ba6e42d3bb2325cdf74069c0ac3c7f1
mcburbulicious <greta.venckunaite@vizlib.com>
2023-07-27 21:06:25 +0300

    more styling

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -1,6 +1,5 @@
 import { getProject } from "@/sanity/sanity-utils";
 import { PortableText } from "@portabletext/react";
-import Image from "next/image";
 
 type Props = {
     params: {
@@ -13,32 +12,33 @@ export default async function Project({ params }: Props) {
 
     return (
         <div className="">
-            <div className="w-full h-[500px]"></div>
-            <h1 className="text-black text-3xl lg:text-5xl font-extrabold leading-small">
-                {project.name}
-            </h1>
-            <div className="text-lg text-gray-700 mt-5 block">
-                <PortableText value={project.content} />
+            <div className="w-full flex justify-center items-center h-[250px] md:h-[350px] lg:h-[450px] relative rounded-xl overflow-hidden mb-12">
+                <img
+                    src={project.image}
+                    alt={project.name}
+                    className="w-full object-cover absolute h-full t-0 l-0 z-0"
+                />
+                <div className="w-full absolute h-full t-0 l-0 z-10 bg-black bg-opacity-60"></div>
+                <h1 className="text-white text-4xl text-center md:text-5xl lg:text-7xl font-extrabold leading-small z-20 px-8">
+                    {project.name}
+                </h1>
             </div>
-            {project.buttonText && (
-                <a
-                    href={project.url}
-                    title="View project"
-                    target="_blank"
-                    rel="noopener noreferrer "
-                    className="bg-pink-500 rounded-lg text-white font-bold py-3 px-4 whitespace-nowrap hover:bg-pink-700  transition-all flex w-fit mt-8"
-                >
-                    {project.buttonText}
-                </a>
-            )}
-
-            <Image
-                src={project.image}
-                alt={project.name}
-                width={1920}
-                height={1080}
-                className="mt-10 rounded-xl object-cover"
-            />
+            <div className="max-w-4xl mx-auto px-4">
+                <div className="text-lg text-gray-700 mt-5 block ">
+                    <PortableText value={project.content} />
+                </div>
+                {project.buttonText && (
+                    <a
+                        href={project.url}
+                        title="View project"
+                        target="_blank"
+                        rel="noopener noreferrer "
+                        className="bg-pink-500 rounded text-white uppercase py-3 px-6 whitespace-nowrap hover:bg-pink-700  transition-all flex w-fit mt-8 mx-auto"
+                    >
+                        {project.buttonText}
+                    </a>
+                )}
+            </div>
         </div>
     );
 }
