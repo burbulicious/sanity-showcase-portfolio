39217bc167f54fce11b8842ab4b55646174b2cb6
mcburbulicious <greta.venckunaite@vizlib.com>
2023-07-27 09:36:59 +0300

    Styling 2

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default async function Page({ params }: Props) {
     const page = await getPage(params.slug);
     return (
-        <div>
+        <div className="w-full rounded-xl bg-gray-100 px-6 md:px-12 py-12 md:py-20">
             <div>
                 <h1 className="text-black text-3xl lg:text-5xl font-extrabold leading-small">
                     {page.title}
